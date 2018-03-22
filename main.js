57db2d8e1ab2b11de90e2467bd5e37ab4f6959e4
Denise <denise.hartog@hotmail.com>
2018-03-22 12:13:36 +0100

    tijd

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var mainHeading = document.getElementById('mainHeading');
 
 var tl1 = new TimelineMax({repeat:24});
-tl1.to(mainHeading, 60, {right: 100});
+tl1.to(mainHeading, 30, {right: 100});
 //tl1.to(mainHeading, 1, {top: 200, ease:Power0.easeOut});
 //tl1.to(mainHeading, 1, {top: 200, ease:Bounce.easeOut});
 //tl1.to(mainHeading, 1, {top: 200, ease:Elastic.easeOut});
