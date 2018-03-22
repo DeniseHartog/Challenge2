056da47fb29a93514a27488cead2541f2c2c7f18
Denise <denise.hartog@hotmail.com>
2018-03-22 11:41:30 +0100

    aanpassingen

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var mainHeading = document.getElementById('mainHeading');
+
+var tl1 = new TimelineMax({repeat:24});
+tl1.to(mainHeading, 60, {right: 100});
+//tl1.to(mainHeading, 1, {top: 200, ease:Power0.easeOut});
+//tl1.to(mainHeading, 1, {top: 200, ease:Bounce.easeOut});
+//tl1.to(mainHeading, 1, {top: 200, ease:Elastic.easeOut});
+//tl1.to(mainHeading, 1, {top: 200, ease:Elastic.easeIn});
+
+
+/*
+https://greensock.com/docs/Easing
+*/
+
+
---
+++ scripts/main2.js
@@ -0,0 +1,19 @@
+
+
+
+var today = new Date();
+
+document.getElementById('message').innerHTML = today.toString();
+
+//document.getElementById('message').innerHTML = today.getMonth();
+//document.getElementById('message').innerHTML = today.getYear();
+//document.getElementById('message').innerHTML = today.getFullYear();
+//document.getElementById('message').innerHTML = today.getDate();
+//document.getElementById('message').innerHTML = today.getDay();
+//document.getElementById('message').innerHTML = today.getHours();
+//document.getElementById('message').innerHTML = today.getMinutes();
+//document.getElementById('message').innerHTML = today.getSeconds();
+//document.getElementById('message').innerHTML = today.getMilliseconds();
+//document.getElementById('message').innerHTML = today.getTime();
+
+
